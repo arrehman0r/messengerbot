f3b562ed5b23f98e741c3294947199e1cd056143
Abdul Rehman <arrehman0r@gmail.com>
2023-09-02 13:04:59 +0500

    now

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,7 @@ router.get("/", function (_req, res) {
   res.send("Hello from bot once again yes!!");
 });
 router.get("/privacy-policy", function (_req, res) {
-  res.sendFile(__dirname + "./../pulic/privacy_policy/html");
+  res.sendFile(__dirname + "./../pulic/privacy_policy.html");
 });
 router.get("/terms-and-services", function (_req, res) {
   res.sendFile(__dirname + "./../public/terms.html");
