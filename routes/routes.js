240dd8b5679c5a9b100bfda3feaf71c5275e5158
Abdul Rehman <arrehman0r@gmail.com>
2023-09-02 12:57:29 +0500

    routes of public

--- routes/routes.js
+++ routes/routes.js
@@ -4,5 +4,10 @@ const router = express.Router();
 router.get("/", function (_req, res) {
   res.send("Hello from bot once again");
 });
-
+router.get("/privacy-policy", function (_req, res) {
+  res.sendFile(__dirname + "/public/privacy_policy.html");
+});
+router.get("/terms-and-services", function (_req, res) {
+  res.sendFile(__dirname + "/public/terms.html");
+});
 module.exports = router;
