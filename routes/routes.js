a6038b73861b2d7e6315c8ac9d9f50b307c9ff67
Abdul Rehman <arrehman0r@gmail.com>
2023-09-02 13:09:21 +0500

    check now

--- routes/routes.js
+++ routes/routes.js
@@ -4,10 +4,19 @@ const router = express.Router();
 router.get("/", function (_req, res) {
   res.send("Hello from bot once again yes!!");
 });
+
 router.get("/privacy-policy", function (_req, res) {
-  res.sendFile(__dirname + "./../public/privacy_policy.html");
+  // Use path.join to construct file path
+  const privacyPolicyPath = path.join(
+    __dirname,
+    "../public/privacy_policy.html"
+  );
+  res.sendFile(privacyPolicyPath);
 });
+
 router.get("/terms-and-services", function (_req, res) {
-  res.sendFile(__dirname + "./../public/terms.html");
+  // Use path.join to construct file path
+  const termsPath = path.join(__dirname, "../public/terms.html");
+  res.sendFile(termsPath);
 });
 module.exports = router;
